4277095d8ed71cf3a1c502d80b7df731a75412bf
Amandeep <samandeepkaur34@gmail.com>
2024-04-11 02:17:10 -0400

    added script file and got html elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector("#city-input");
+const searchButton = document.querySelector("#search-btn");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const daysForecastDiv = document.querySelector(".days-forecast");
+const searchHistoryDiv = document.querySelector("#search-history");
+
+const API_KEY = '2db373437ba37df2b7f9ee7d7bf44dff'; 
